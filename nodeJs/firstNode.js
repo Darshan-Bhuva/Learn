f121e0e48d77dc12998c62b4b07d3ce7b6f87387
KelvinPansuriya08 <kelvinpansuriya008@gmail.com>
2023-01-05 18:44:14 +0530

    try and catch, node.js REPL learn

--- nodeJs/firstNode.js
+++ nodeJs/firstNode.js
@@ -1,2 +1,25 @@
+// How To Run Program Of node.js In terminal :- Write node 'Enter File Name, Which You Want To Run'
+// type nul > "Enter File Name For Create New File Using Command"
+// The REPL Feature Of Node Is Very Useful In Experimenting With Node.js Codes And To Debug JavaScript Codes.
+// How To Trun On REPL Mode In Node.js.... ANSWER :- In Vs Code Terminal Or Any Other Terminal Write 'node'
+// Turn On REPL MODE And Press "Tab Key" Twice, For Reason See All The Properties And Methods Of Node.js
+// Use Of REPL ------- {In Terminal Of Vs Code Or Any Other Terminal}
+
+// 1. Js Expression
+// 2. Use Variables
+// 3. Multiline Code
+// 4. Use (_) To Get The Last Result
+// 5. We Can Use Editor Mode
+
 let newName = "node js";
 console.log(`Hello World! ${newName}`);
+
+console.log("What Is A REPL In Node Js");
+console.log(
+  "What Is A REPL In Node Js",
+  "ANSWER :- Write 'node' In Terminal Of VS Code Or Any Other Terminal"
+);
+console.log("R :- Read");
+console.log("E :- Eval");
+console.log("P :- Print");
+console.log("L :- Loop");
